2dd6f83f34bf03df2b9a3c2af66fcbf872b6232b
Tatsuya Namikwa <m757e888a7n7r@gmaiil.com>
2024-02-04 02:04:26 +0900

    ホームページ作成開始

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div>© 2024 Tatsuya Namikawa. All Rights Reserved.</div>;
+};
+
+export default Footer;
---
+++ src/features/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ResponsiveAppBar from "components/ResponsiveAppBar";
+import Footer from "components/Footer";
+
+export const Home = () => {
+  return (
+    <>
+      <ResponsiveAppBar />
+      <Footer />
+    </>
+  );
+};
